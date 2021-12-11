c6cd5ab85cb703f0800767c30a479174c3918a59
vivek lokhande <vivek2811999@gmail.com>
2021-12-11 17:50:53 +0530

    added route without route params

--- node-project/node-express-course/02-express-tutorial/app.js
+++ node-project/node-express-course/02-express-tutorial/app.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
-app.listen(6000, () => {
-  console.log("server is listening on port 6000");
+const { products } = require("./data");
+
+app.get("/", (req, res) => {
+  res.send("<h1> Home Page </h1><a href='/api/products'>products</a>");
 });
 
-//app.get
-//app.post
-//app.put
-//app.delete
-//app.all
-//app.use
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+
+app.listen(5113, () => {
+  console.log("SERVER is listening on port 5113...");
+});
