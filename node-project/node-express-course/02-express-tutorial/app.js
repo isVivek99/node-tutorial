948f7a7c64638d2bffea2f80a33b63df4477a51d
vivek lokhande <vivek2811999@gmail.com>
2021-12-13 12:34:48 +0530

    changed the params-query file

--- node-project/node-express-course/02-express-tutorial/app.js
+++ node-project/node-express-course/02-express-tutorial/app.js
@@ -5,7 +5,7 @@ const { products } = require("./data");
 app.get("/", (req, res) => {
   res.send("<h1> Home Page </h1><a href='/api/products'>products</a>");
 });
-
+//route params-start
 app.get("/api/products", (req, res) => {
   const newProducts = products.map((product) => {
     const { id, name, image } = product;
@@ -28,7 +28,9 @@ app.get("/api/products/:productID", (req, res) => {
 
   return res.json(singleProduct);
 });
+//route params-end
 
+//queryString-start
 app.get("/api/v1/query", (req, res) => {
   console.log(req.query);
   const { search, limit } = req.query;
@@ -46,9 +48,13 @@ app.get("/api/v1/query", (req, res) => {
   if (limit) {
     sortedProducts = sortedProducts.slice(0, Number(limit));
   }
-
+  if (sortedProducts.length < 1) {
+    //return res.status(200).send("No products matched your search!");
+    return res.status(200).json({ sucess: true, data: [] });
+  }
   return res.status(200).json(sortedProducts);
 });
+//queryString-end
 
 app.listen(5113, () => {
   console.log("SERVER is listening on port 5113...");
