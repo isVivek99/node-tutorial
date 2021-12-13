335227f9c9774a1b6ef1551c17181b483b65b42c
vivek lokhande <vivek2811999@gmail.com>
2021-12-13 12:08:17 +0530

    added string query parameters and sent responses accordingly

--- node-project/node-express-course/02-express-tutorial/app.js
+++ node-project/node-express-course/02-express-tutorial/app.js
@@ -11,7 +11,43 @@ app.get("/api/products", (req, res) => {
     const { id, name, image } = product;
     return { id, name, image };
   });
+
   res.json(newProducts);
+});
+
+app.get("/api/products/:productID", (req, res) => {
+  console.log(req.params);
+  const { productID } = req.params;
+
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product does not exist!");
+  }
+
+  return res.json(singleProduct);
+});
+
+app.get("/api/v1/query", (req, res) => {
+  console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    console.log("here1");
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+
+    sortedProducts = sortedProducts.slice(0, Number(1));
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+
+  return res.status(200).json(sortedProducts);
 });
 
 app.listen(5113, () => {
