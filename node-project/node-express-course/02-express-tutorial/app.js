80835f494a7f5acb56eb342ef156a013fba4dfc8
vivek lokhande <vivek2811999@gmail.com>
2021-12-13 16:39:37 +0530

    added middleware functions and use then for various endpoints

--- node-project/node-express-course/02-express-tutorial/app.js
+++ node-project/node-express-course/02-express-tutorial/app.js
@@ -1,60 +1,22 @@
 const express = require("express");
 const app = express();
-const { products } = require("./data");
+const logger = require("./logger");
+const authorize = require("./authorize");
+//req => middleware => res
+app.use("/", [logger, authorize]);
 
 app.get("/", (req, res) => {
-  res.send("<h1> Home Page </h1><a href='/api/products'>products</a>");
+  res.send("home");
 });
-//route params-start
+app.get("/about", (req, res) => {
+  res.send("About");
+});
 app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-
-  res.json(newProducts);
+  res.send("products");
 });
-
-app.get("/api/products/:productID", (req, res) => {
-  console.log(req.params);
-  const { productID } = req.params;
-
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  );
-  if (!singleProduct) {
-    return res.status(404).send("Product does not exist!");
-  }
-
-  return res.json(singleProduct);
+app.get("/api/items", (req, res) => {
+  res.send("items");
 });
-//route params-end
-
-//queryString-start
-app.get("/api/v1/query", (req, res) => {
-  console.log(req.query);
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    console.log("here1");
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-
-    sortedProducts = sortedProducts.slice(0, Number(1));
-  }
-
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-  if (sortedProducts.length < 1) {
-    //return res.status(200).send("No products matched your search!");
-    return res.status(200).json({ sucess: true, data: [] });
-  }
-  return res.status(200).json(sortedProducts);
-});
-//queryString-end
 
 app.listen(5113, () => {
   console.log("SERVER is listening on port 5113...");
