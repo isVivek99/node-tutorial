107ba61891913e17c29e5de70d9fef03b9d19b09
vivek lokhande <vivek2811999@gmail.com>
2021-12-14 16:01:39 +0530

    added middleware function s

--- node-project/node-express-course/02-express-tutorial/app.js
+++ node-project/node-express-course/02-express-tutorial/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 const logger = require("./logger");
 const authorize = require("./authorize");
 //req => middleware => res
-app.use("/", [logger, authorize]);
+// app.use("/", [logger, authorize]);
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("home");
@@ -15,6 +17,7 @@ app.get("/api/products", (req, res) => {
   res.send("products");
 });
 app.get("/api/items", (req, res) => {
+  console.log(req.user);
   res.send("items");
 });
 
