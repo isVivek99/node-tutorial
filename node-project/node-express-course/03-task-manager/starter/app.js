218d621f90eacfa7ae136c3b25b3c3862bc99e49
vivek lokhande <vivek2811999@gmail.com>
2021-12-17 17:07:39 +0530

    setup the mongodb datatbase

--- node-project/node-express-course/03-task-manager/starter/app.js
+++ node-project/node-express-course/03-task-manager/starter/app.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+//middleware; we get the object in req.body
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`Server is listening opn port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (err) {
+    console.log(err);
+  }
+};
+start();
