c9ff9b818a92b56e65b01a63da1261eda480d4c4
vivek lokhande <vivek2811999@gmail.com>
2021-12-30 18:23:52 +0530

    setting up the controller

--- node-project/node-express-course/04-store-api/starter/controllers/products.js
+++ node-project/node-express-course/04-store-api/starter/controllers/products.js
@@ -1,8 +1,52 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "products testing route" });
+  const products = await Product.find({})
+    .sort("name")
+    .select("name price")
+    .skip(5);
+  res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const { featured, company, name, sort, fields } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
+  console.log(queryObject);
+
+  let result = Product.find(queryObject);
+  // sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+    console.log("here");
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
+  console.log(typeof products[1].price);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
